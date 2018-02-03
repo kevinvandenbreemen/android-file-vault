e4f01dfc9f69f8a98dcebbfa2866e031c595f077
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-02-03 15:26:41 -0500

    Validate android system logger

---
+++ app/src/test/java/com/vandenbreemen/mobilesecurestorage/log/AndroidSystemLogTest.java
@@ -0,0 +1,34 @@
+package com.vandenbreemen.mobilesecurestorage.log;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * <h2>Intro</h2>
+ * <p>Validate behaviour of the system log with the {@link AndroidSystemLog}
+ * <h2>Other Details</h2>
+ *
+ * @author kevin
+ */
+@RunWith(RobolectricTestRunner.class)
+public class AndroidSystemLogTest {
+
+    @BeforeClass
+    public static void setup(){
+        SystemLog.setGlobalSystemLog(new AndroidSystemLog());
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        SystemLog.setGlobalSystemLog(new SystemOutLog());
+    }
+
+    @Test
+    public void testLog(){
+        SystemLog.get().info("This is a test");
+    }
+
+}
