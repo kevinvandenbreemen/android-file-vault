a927d0711b00e1d225b3224332327ff772f41799
kevin <kevin.vandenbreemen+github@gmail.com>
2018-02-09 19:02:06 -0500

    Start work on the file activity itself

--- app/src/main/java/com/vandenbreemen/mobilesecurestorage/android/FileSelectActivity.kt
+++ app/src/main/java/com/vandenbreemen/mobilesecurestorage/android/FileSelectActivity.kt
@@ -1,14 +1,49 @@
 package com.vandenbreemen.mobilesecurestorage.android
 
+import android.Manifest
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import com.vandenbreemen.mobilesecurestorage.R
 import com.vandenbreemen.mobilesecurestorage.android.mvp.FileSelectView
 
-class FileSelectActivity : Activity(), FileSelectView {
+class FileSelectActivity : Activity(), FileSelectView, ActivityCompat.OnRequestPermissionsResultCallback {
+
+    companion object {
+        /**
+         * Permissions for file IO
+         */
+        const val PERM_REQUEST_ID = 2501
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_file_select)
+
+        //  Check for file IO permissions
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+             PERM_REQUEST_ID)
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray){
+        when(requestCode){
+            PERM_REQUEST_ID->{
+                if((grantResults!!.isNotEmpty() && grantResults!![0] == PackageManager.PERMISSION_GRANTED)){
+
+                }
+            }
+        }
+    }
+
+    /*
+
+     */
+
 }
