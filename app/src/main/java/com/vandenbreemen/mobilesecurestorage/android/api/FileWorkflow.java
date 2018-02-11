94da68857871e7d5a27824a79ee81654f5b58edb
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-02-10 19:29:00 -0500

    Define persistent workflow for file selection/creation

---
+++ app/src/main/java/com/vandenbreemen/mobilesecurestorage/android/api/FileWorkflow.java
@@ -0,0 +1,49 @@
+package com.vandenbreemen.mobilesecurestorage.android.api;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.File;
+
+/**
+ * <h2>Intro</h2>
+ * <p>
+ * <h2>Other Details</h2>
+ *
+ * @author kevin
+ */
+public class FileWorkflow implements Parcelable {
+
+    public static final Creator<FileWorkflow> CREATOR = new Creator<FileWorkflow>() {
+        @Override
+        public FileWorkflow createFromParcel(Parcel source) {
+            FileWorkflow ret = new FileWorkflow();
+            ret.fileOrDirectory = new File(source.readString());
+            return ret;
+        }
+
+        @Override
+        public FileWorkflow[] newArray(int size) {
+            return new FileWorkflow[0];
+        }
+    };
+    private File fileOrDirectory;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(fileOrDirectory.getAbsolutePath());
+    }
+
+    public File getFileOrDirectory() {
+        return fileOrDirectory;
+    }
+
+    public void setFileOrDirectory(File fileOrDirectory) {
+        this.fileOrDirectory = fileOrDirectory;
+    }
+}
---
+++ app/src/test/java/com/vandenbreemen/mobilesecurestorage/android/api/FileWorkflowTest.java
@@ -0,0 +1,50 @@
+package com.vandenbreemen.mobilesecurestorage.android.api;
+
+import android.os.Environment;
+import android.os.Parcel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.AllOf.allOf;
+
+/**
+ * <h2>Intro</h2>
+ * <p>
+ * <h2>Other Details</h2>
+ *
+ * @author kevin
+ */
+@RunWith(RobolectricTestRunner.class)
+public class FileWorkflowTest {
+
+    @Test
+    public void testWriteFileLocation() {
+
+        File expected = new File(Environment.getExternalStorageDirectory() + File.separator + "test");
+
+        FileWorkflow workflow = new FileWorkflow();
+        workflow.setFileOrDirectory(expected);
+
+        Parcel parcel = Parcel.obtain();
+
+        workflow.writeToParcel(parcel, workflow.describeContents());
+        parcel.setDataPosition(0);
+
+        FileWorkflow fromParcel = FileWorkflow.CREATOR.createFromParcel(parcel);
+        File fileOrDir = fromParcel.getFileOrDirectory();
+
+        assertThat(fileOrDir, allOf(
+                notNullValue(),
+                is(expected)
+        ));
+
+    }
+
+}
