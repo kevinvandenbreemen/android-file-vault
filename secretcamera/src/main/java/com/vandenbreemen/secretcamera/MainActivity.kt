9c15abe10f2593fd30fc51ad7acfe6a64425e4b0
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-03-08 21:36:11 -0500

    Start work on app logic

--- secretcamera/src/main/java/com/vandenbreemen/secretcamera/MainActivity.kt
+++ secretcamera/src/main/java/com/vandenbreemen/secretcamera/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.vandenbreemen.secretcamera
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import com.vandenbreemen.mobilesecurestorage.android.api.FileWorkflow
+import com.vandenbreemen.mobilesecurestorage.android.fragment.SFSNavFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    /**
+     * File access workflow (containing the file we're going to be working with)
+     */
+    var fsWorkflow: FileWorkflow? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //  Get file workflow
+        savedInstanceState?.let {
+            fsWorkflow = it.getParcelable(FileWorkflow.PARM_WORKFLOW_NAME)
+        } ?: run{
+            fsWorkflow = intent.getParcelableExtra(FileWorkflow.PARM_WORKFLOW_NAME)
+        }
+
+        fsWorkflow = fsWorkflow?: FileWorkflow()
+
+        if(fsWorkflow?.fileOrDirectory != null && fsWorkflow?.fileOrDirectory!!.isFile){
+
+        }
+        else{   //  Otherwise show the FS select fragment!
+            val frag = SFSNavFragment()
+            fsWorkflow!!.activityToStartAfterTargetActivityFinished = javaClass
+            savedInstanceState?.let {
+                frag.arguments = it
+            } ?: run{
+                frag.workflow = fsWorkflow!!
+            }
+            frag.setCancelAction(javaClass)
+            fragmentManager.beginTransaction().add(R.id.upperSection, frag).commit()
+        }
+
+
     }
 }
