13b54e28eb4488b395fdc16b2f9b7fe1c14c1630
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-03-11 21:28:43 -0400

    Get the loading test to pass by waiting for the next screen!

--- secretcamera/src/androidTest/java/com/vandenbreemen/secretcamera/MainActivityTest.kt
+++ secretcamera/src/androidTest/java/com/vandenbreemen/secretcamera/MainActivityTest.kt
@@ -1,20 +1,21 @@
 package com.vandenbreemen.secretcamera
 
-import android.support.test.espresso.Espresso
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.IdlingPolicies
+import android.support.test.espresso.IdlingRegistry
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotExist
 import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
 import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo
-import com.schibsted.spain.barista.interaction.BaristaScrollInteractions.scrollTo
+import com.vandenbreemen.secretcamera.util.ElapsedTimeIdlingResource
+import org.junit.After
 import org.junit.Before
 import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
+import java.util.concurrent.TimeUnit
 
 /**
  * <h2>Intro</h2>
@@ -28,15 +29,32 @@ class MainActivityTest {
 
     companion object {
         val DEFAULT_LOCATION = "Music"
+        val TIME_TO_WAIT = TimeUnit.MILLISECONDS.convert(2, TimeUnit.SECONDS)
     }
 
     val activityRule:ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    var waitResource: ElapsedTimeIdlingResource? = null
 
     lateinit var fileName:String
 
     @Before
     fun setup(){
         fileName = "unitTestFile"
+        IdlingPolicies.setMasterPolicyTimeout(60, TimeUnit.SECONDS);
+        IdlingPolicies.setIdlingResourceTimeout(26, TimeUnit.SECONDS);
+    }
+
+    fun getElapsedTimeIdlingResource(): ElapsedTimeIdlingResource {
+        waitResource = ElapsedTimeIdlingResource(TIME_TO_WAIT)
+        return waitResource!!
+    }
+
+    @After
+    fun tearDown(){
+        waitResource?.let {
+            IdlingRegistry.getInstance().unregister(it)
+        }
     }
 
     @Test
@@ -71,6 +89,8 @@ class MainActivityTest {
         clickOn(R.id.ok)
         writeTo(R.id.password, "password")
         clickOn(R.id.ok)
+
+        IdlingRegistry.getInstance().register(getElapsedTimeIdlingResource())
 
         //  Assert
         assertDisplayed(R.id.takePicture)
---
+++ secretcamera/src/androidTest/java/com/vandenbreemen/secretcamera/util/ElapsedTimeIdlingResource.java
@@ -0,0 +1,43 @@
+package com.vandenbreemen.secretcamera.util;
+
+import android.support.test.espresso.IdlingResource;
+
+//  Thanks to Chiuki
+//  https://github.com/chiuki/espresso-samples/blob/a70a343862a4199804f6fdffd9173cd453bb8d11/idling-resource-elapsed-time/app/build.gradle
+/**
+ * <h2>Intro</h2>
+ * <p>
+ * <h2>Other Details</h2>
+ *
+ * @author kevin
+ */
+public class ElapsedTimeIdlingResource implements IdlingResource {
+    private final long startTime;
+    private final long waitingTime;
+    private ResourceCallback resourceCallback;
+
+    public ElapsedTimeIdlingResource(long waitingTime) {
+        this.startTime = System.currentTimeMillis();
+        this.waitingTime = waitingTime;
+    }
+
+    @Override
+    public String getName() {
+        return ElapsedTimeIdlingResource.class.getName() + ":" + waitingTime;
+    }
+
+    @Override
+    public boolean isIdleNow() {
+        long elapsed = System.currentTimeMillis() - startTime;
+        boolean idle = (elapsed >= waitingTime);
+        if (idle) {
+            resourceCallback.onTransitionToIdle();
+        }
+        return idle;
+    }
+
+    @Override
+    public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
+        this.resourceCallback = resourceCallback;
+    }
+}
