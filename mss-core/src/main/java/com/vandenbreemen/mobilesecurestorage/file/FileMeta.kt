c02222ff30324a60fafecbc02121d6203a08cf0d
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-03-18 10:42:07 -0400

    Optimize storage of file type bytes

--- mss-core/src/main/java/com/vandenbreemen/mobilesecurestorage/file/FileMeta.kt
+++ mss-core/src/main/java/com/vandenbreemen/mobilesecurestorage/file/FileMeta.kt
@@ -3,17 +3,33 @@ package com.vandenbreemen.mobilesecurestorage.file
 import com.vandenbreemen.mobilesecurestorage.file.api.FileType
 import com.vandenbreemen.mobilesecurestorage.file.api.FileTypes
 import com.vandenbreemen.mobilesecurestorage.file.api.getBytes
+import java.io.Serializable
 
-class FileMeta {
+class FileMeta : Serializable {
 
-    private var fileTypeBytes:Array<Byte?> = arrayOf(FileTypes.UNKNOWN.firstByte, FileTypes.UNKNOWN.secondByte)
+    private var fileTypeBytes: ByteArray = ByteArray(2, { it -> if (it == 0) FileTypes.UNKNOWN.firstByte!! else FileTypes.UNKNOWN.secondByte!! })
 
     fun setFileType(fileType:FileType){
-        this.fileTypeBytes = fileType.getBytes()
+        val byteArray = fileType.getBytes()
+        this.fileTypeBytes = ByteArray(byteArray.size, { index -> byteArray[index]!! })
     }
 
     fun getFileType(): FileType {
-        return FileTypes.getFileType(fileTypeBytes)!!
+        val bytes = Array<Byte?>(fileTypeBytes.size, { it -> fileTypeBytes[it] })
+        return FileTypes.getFileType(bytes)!!
+
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is FileMeta) {
+            if (other.fileTypeBytes.size != this.fileTypeBytes.size) {
+                return false
+            }
+
+            return other.fileTypeBytes contentEquals this.fileTypeBytes
+
+        }
+        return false
     }
 
 }
--- mss-core/src/test/java/com/vandenbreemen/mobilesecurestorage/file/FileMetaTest.kt
+++ mss-core/src/test/java/com/vandenbreemen/mobilesecurestorage/file/FileMetaTest.kt
@@ -1,11 +1,14 @@
 package com.vandenbreemen.mobilesecurestorage.file
 
+import com.vandenbreemen.mobilesecurestorage.TestConstants
+import com.vandenbreemen.mobilesecurestorage.data.Serialization
 import com.vandenbreemen.mobilesecurestorage.file.api.FileType
 import com.vandenbreemen.mobilesecurestorage.file.api.FileTypes
 import junit.framework.Assert
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
 import org.junit.Test
-import kotlin.reflect.KClass
+import java.nio.charset.Charset
 
 /**
  * <br/>Created by kevin on 17/03/18.
@@ -35,6 +38,49 @@ class FileMetaTest {
         fileMeta.setFileType(FakeFileType.TEST_FILE_TYPE)
 
         assertEquals("Additional file type", FakeFileType.TEST_FILE_TYPE, fileMeta.getFileType())
+    }
+
+    @Test
+    fun shouldBeAbleToStoreToFile() {
+        val fileMeta = FileMeta()
+        fileMeta.setFileType(FileTypes.SYSTEM)
+
+        val indexedFile = IndexedFile(TestConstants.getTestFile("storeFileMeta"))
+        indexedFile.storeObject("test", fileMeta)
+    }
+
+    @Test
+    fun shouldSerialize() {
+        val fileMeta = FileMeta()
+        fileMeta.setFileType(FileTypes.SYSTEM)
+        println(Serialization.toBytes(fileMeta).toString(Charset.defaultCharset()))
+    }
+
+    @Test
+    fun shouldDeserialize() {
+        val fileMeta = FileMeta()
+        fileMeta.setFileType(FileTypes.SYSTEM)
+
+        val serialized = Serialization.toBytes(fileMeta)
+
+        val retrieved = Serialization.deserializeBytes(serialized) as FileMeta
+        assertNotNull("retrieved", retrieved)
+        assertEquals("same data", fileMeta, retrieved)
+        assertEquals("Same type", FileTypes.SYSTEM, retrieved.getFileType())
+
+    }
+
+    @Test
+    fun shouldBeAbleToRetrieveFromFile() {
+        val fileMeta = FileMeta()
+        fileMeta.setFileType(FileTypes.SYSTEM)
+
+        val indexedFile = IndexedFile(TestConstants.getTestFile("retrievedFileMeta"))
+        indexedFile.storeObject("test", fileMeta)
+
+        val retrieved = indexedFile.loadFile("test") as FileMeta
+        assertEquals("same data", fileMeta, retrieved)
+        assertEquals("Same type", FileTypes.SYSTEM, retrieved.getFileType())
     }
 
 }
