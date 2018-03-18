cff0beddfbef3873dc0da22c4ea604846aa3df08
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-03-18 14:30:06 -0400

    Get the basics working

--- secretcamera/src/main/java/com/vandenbreemen/secretcamera/StringSelectorActivity.kt
+++ secretcamera/src/main/java/com/vandenbreemen/secretcamera/StringSelectorActivity.kt
@@ -1,24 +1,78 @@
 package com.vandenbreemen.secretcamera
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+
+const val SELECTED_STRING = "__SELECTED_STRING"
+
+class StringSelectorWorkflow() : Parcelable {
+
+    constructor(activity: Class<out Activity>) : this() {
+        this.activityClassStr = activity.canonicalName
+    }
+
+    var activityClassStr: String = ""
+
+    lateinit var selected: String
+
+    constructor(parcel: Parcel) : this() {
+        activityClassStr = parcel.readString()
+        selected = parcel.readString()
+    }
+
+    fun getActivityClass(): Class<in Activity> = Class.forName(activityClassStr) as Class<in Activity>
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(activityClassStr)
+        parcel.writeString(selected)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<StringSelectorWorkflow> {
+        override fun createFromParcel(parcel: Parcel): StringSelectorWorkflow {
+            return StringSelectorWorkflow(parcel)
+        }
+
+        override fun newArray(size: Int): Array<StringSelectorWorkflow?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
 
 class StringSelectorActivity : Activity() {
 
     companion object {
         const val LIST = "__ITEM_LIST"
+        const val WORKFLOW = "__WORKFLOW"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_string_selector)
 
-        val items = intent.getStringArrayListExtra(LIST)
+        val workflow = intent.getParcelableExtra<StringSelectorWorkflow>(WORKFLOW)!!
+        val items = intent.getStringArrayListExtra(LIST)!!
+
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
-        findViewById<ListView>(R.id.itemList).setAdapter(adapter)
+        val listView = findViewById<ListView>(R.id.itemList)
+        listView.setAdapter(adapter)
+        listView.setOnItemClickListener(AdapterView.OnItemClickListener({ parent, view, position, id ->
+            val selected = adapter.getItem(position)
+            val intent = Intent(this, workflow.getActivityClass())
+            intent.putExtra(SELECTED_STRING, selected)
+            startActivity(intent)
+        }))
     }
 
     fun onOk(view: View) {
--- secretcamera/src/test/java/com/vandenbreemen/secretcamera/StringSelectorActivityTest.kt
+++ secretcamera/src/test/java/com/vandenbreemen/secretcamera/StringSelectorActivityTest.kt
@@ -2,6 +2,10 @@ package com.vandenbreemen.secretcamera
 
 import android.content.Intent
 import android.widget.ListView
+import com.vandenbreemen.mobilesecurestorage.android.FileSelectActivity
+import com.vandenbreemen.secretcamera.StringSelectorActivity.Companion.WORKFLOW
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Rule
 import org.junit.Test
@@ -30,6 +34,7 @@ class StringSelectorActivityTest {
         val intent = Intent(ShadowApplication.getInstance().applicationContext, StringSelectorActivity::class.java)
         intent.putStringArrayListExtra(StringSelectorActivity.LIST,
                 ArrayList<String>(Arrays.asList("Larry", "Curly", "Moe")))
+        intent.putExtra(WORKFLOW, StringSelectorWorkflow())
 
         val activity = buildActivity(StringSelectorActivity::class.java, intent)
                 .create()
@@ -43,6 +48,34 @@ class StringSelectorActivityTest {
         errorCollector.checkThat(listView.adapter.getItem(0) as String, `is`("Larry"))
         errorCollector.checkThat(listView.adapter.getItem(1) as String, `is`("Curly"))
         errorCollector.checkThat(listView.adapter.getItem(2) as String, `is`("Moe"))
+
+    }
+
+    @Test
+    fun shouldKickOffActivityWithSelection() {
+        val intent = Intent(ShadowApplication.getInstance().applicationContext, StringSelectorActivity::class.java)
+        intent.putStringArrayListExtra(StringSelectorActivity.LIST,
+                ArrayList<String>(Arrays.asList("Larry", "Curly", "Moe")))
+        intent.putExtra(StringSelectorActivity.WORKFLOW, StringSelectorWorkflow(FileSelectActivity::class.java))
+
+        val activity = buildActivity(StringSelectorActivity::class.java, intent)
+                .create()
+                .get()
+
+        val listView = activity.findViewById<ListView>(R.id.itemList)
+        val shadow = shadowOf(listView)
+        shadow.populateItems()
+
+        shadow.performItemClick(1)
+
+        val shadowAct = shadowOf(activity)
+        val nextActivity = shadowAct.nextStartedActivity
+
+        assertNotNull("Intent started", nextActivity)
+
+        val nextShadow = shadowOf(nextActivity)
+        assertEquals("Next activity", FileSelectActivity::class.java, nextShadow.intentClass)
+        errorCollector.checkThat(nextActivity.getStringExtra(SELECTED_STRING), `is`("Curly"))
 
     }
 
