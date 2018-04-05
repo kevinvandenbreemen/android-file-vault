218a54cee43530c4c0382938f37d5eb9b96fea53
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-04-04 20:39:03 -0400

    Get basic import logic done

--- mss-core/src/main/java/com/vandenbreemen/mobilesecurestorage/android/mvp/importfiles/FileImportModel.kt
+++ mss-core/src/main/java/com/vandenbreemen/mobilesecurestorage/android/mvp/importfiles/FileImportModel.kt
@@ -1,7 +1,41 @@
 package com.vandenbreemen.mobilesecurestorage.android.mvp.importfiles
 
+import com.vandenbreemen.mobilesecurestorage.android.sfs.SFSCredentials
+import com.vandenbreemen.mobilesecurestorage.message.ApplicationError
+import com.vandenbreemen.mobilesecurestorage.patterns.mvp.Model
+import io.reactivex.Single
+import io.reactivex.SingleOnSubscribe
+import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
+import io.reactivex.schedulers.Schedulers.computation
+import java.io.File
+
 /**
  * Created by kevin on 04/04/18.
  */
-class FileImportModel {
+class FileImportModel(credentials: SFSCredentials) : Model(credentials) {
+
+    /**
+     *
+     */
+    private val fileSystemInteractor = FileSystemInteractorImpl()
+
+    override fun onClose() {
+
+    }
+
+    override fun setup() {
+
+    }
+
+    fun importDir(directoryToImport: File): Single<Unit> {
+        return fileSystemInteractor.listFiles(directoryToImport).flatMap { files: List<File>? ->
+            Single.create(SingleOnSubscribe<Unit> { emitter ->
+                files?.let {
+                    it.forEach({ fileToImport -> sfs.importFile(fileToImport, fileToImport.name) })
+                    emitter.onSuccess(Unit)
+                } ?: run { emitter.onError(ApplicationError("Unknown error importing files")) }
+            }).observeOn(computation()).subscribeOn(mainThread())
+        }
+
+    }
 }
---
+++ mss-core/src/test/java/com/vandenbreemen/mobilesecurestorage/android/mvp/importfiles/FileImportModelTest.kt
@@ -0,0 +1,68 @@
+package com.vandenbreemen.mobilesecurestorage.android.mvp.importfiles
+
+import android.os.Environment
+import com.vandenbreemen.mobilesecurestorage.android.sfs.SFSCredentials
+import com.vandenbreemen.mobilesecurestorage.security.SecureString
+import com.vandenbreemen.mobilesecurestorage.security.crypto.persistence.SecureFileSystem
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.plugins.RxJavaPlugins
+import junit.framework.TestCase.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.io.File
+
+/**
+ * Created by kevin on 04/04/18.
+ */
+@RunWith(RobolectricTestRunner::class)
+class FileImportModelTest {
+
+    lateinit var directoryToImport: File
+
+    lateinit var sfsFile: File
+
+    @Before
+    fun setup() {
+        RxJavaPlugins.setIoSchedulerHandler { scheduler -> AndroidSchedulers.mainThread() }
+        RxJavaPlugins.setComputationSchedulerHandler { scheduler -> AndroidSchedulers.mainThread() }
+        directoryToImport = File(Environment.getExternalStorageDirectory().toString() + File.separator + "toImport")
+        directoryToImport.mkdir()
+        sfsFile = File(Environment.getExternalStorageDirectory().toString() + File.separator + "sfs")
+    }
+
+    fun createPassword(): SecureString {
+        return SecureFileSystem.generatePassword(SecureString.fromPassword("password"))
+    }
+
+    fun sfs(): SecureFileSystem {
+        return object : SecureFileSystem(sfsFile) {
+            override fun getPassword(): SecureString {
+                return createPassword()
+            }
+
+        }
+    }
+
+    @Test
+    fun shouldImportFiles() {
+
+        //  Arrange
+        val sfsCredentials = SFSCredentials(sfsFile, createPassword())
+        File(directoryToImport.absolutePath + File.separator + "newfile").createNewFile()
+        File(directoryToImport.absolutePath + File.separator + "newfile1").createNewFile()
+
+        //  Act
+        val fileImportModel = FileImportModel(sfsCredentials)
+        fileImportModel.init().subscribe()
+        fileImportModel.importDir(directoryToImport).test()
+                .assertComplete()
+                .assertNoErrors()
+
+        //  assert
+        assertEquals("Single file imported", 2, sfs().listFiles().size)
+
+    }
+
+}
