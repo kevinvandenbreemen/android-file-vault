1261c6b0dc1ef6f7a7f8b04f26f4b5c804dfa451
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-04-08 20:34:16 -0400

    Basics first:  Create file.  Still struggling with deleting

--- mss-core/src/androidTest/java/com/vandenbreemen/mobilesecurestorage/android/ADBFunctionalityLearningTest.kt
+++ mss-core/src/androidTest/java/com/vandenbreemen/mobilesecurestorage/android/ADBFunctionalityLearningTest.kt
@@ -2,49 +2,61 @@ package com.vandenbreemen.mobilesecurestorage.android
 
 import android.Manifest
 import android.os.Environment
-import android.support.test.InstrumentationRegistry
 import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
-import android.util.Log
-import com.vandenbreemen.mobilesecurestorage.TestResources
-import junit.framework.TestCase.assertTrue
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Before
+import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ErrorCollector
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 import java.io.File
 
 /**
  * <h2>Intro</h2>
- *
+ * Trying to figure out how to delete test files:
+ * <br/>See https://stackoverflow.com/questions/49363485/how-to-put-test-data-files-on-a-device-for-instrumental-tests
  * <h2>Other Details</h2>
  * @author kevin
  */
 @RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ADBFunctionalityLearningTest {
 
     companion object {
         val TAG = "LearningTests"
+        val testFilename = "testData_${System.currentTimeMillis()}"
     }
 
     //  Set up storage/reading permissions
     @get:Rule
     val permissions = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
 
+    @get:Rule
+    val errorCollector: ErrorCollector = ErrorCollector()
+
+    lateinit var testFilename: String
+
+    @Before
+    fun setup() {
+        this.testFilename = "testData_${System.currentTimeMillis()}"
+    }
 
     @Test
-    fun testUploadFileToDevice() {
-
-        val localFile = TestResources.TEST_RES_IMG_1
-        val command = "push $localFile ${Environment.getExternalStorageDirectory().absolutePath}"
-        Log.d(TAG, "Executing command:  $command")
-        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand(
-                command
-        )
-
-        //  Now list the files
-        val updaloaded = File(Environment.getExternalStorageDirectory().absolutePath + "/" +
-                TestResources.FILENAME_RES_IMG_1)
-        assertTrue("File uploaded:  ${updaloaded.absolutePath}", updaloaded.exists())
+    fun howToCreateANewFileOnDevice() {
+        val newFile = File(Environment.getExternalStorageDirectory().absolutePath + File.separator + testFilename)
+        errorCollector.checkThat(newFile.createNewFile(), `is`(true))
+        errorCollector.checkThat(newFile.exists(), `is`(true))
     }
+
+    @Test
+    fun howToDeleteTestData() {
+        val newFile = File(Environment.getExternalStorageDirectory().absolutePath + File.separator + testFilename)
+        errorCollector.checkThat(newFile.delete(), `is`(true))  //  Won't delete the file....
+        errorCollector.checkThat(newFile.exists(), `is`(false))
+    }
+
 
 }
