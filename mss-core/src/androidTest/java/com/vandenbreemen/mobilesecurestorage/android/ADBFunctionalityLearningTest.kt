58dbb14d79e828053a09606b5bf79aa470c4e25b
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-04-14 20:20:26 -0400

    Demonstrate creating test images (I hope)

--- mss-core/src/androidTest/java/com/vandenbreemen/mobilesecurestorage/android/ADBFunctionalityLearningTest.kt
+++ mss-core/src/androidTest/java/com/vandenbreemen/mobilesecurestorage/android/ADBFunctionalityLearningTest.kt
@@ -1,11 +1,16 @@
 package com.vandenbreemen.mobilesecurestorage.android
 
 import android.Manifest
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Environment
+import android.support.test.InstrumentationRegistry.getContext
 import android.support.test.InstrumentationRegistry.getInstrumentation
 import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
 import android.util.Log
+import com.vandenbreemen.mobilesecurestorage.R
+import junit.framework.TestCase.assertNotNull
 import org.awaitility.Awaitility.await
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.Matchers.greaterThan
@@ -16,11 +21,13 @@ import org.junit.Test
 import org.junit.rules.ErrorCollector
 import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.ObjectOutputStream
 import java.security.SecureRandom
 import java.util.concurrent.TimeUnit
+
 
 /**
  * <h2>Intro</h2>
@@ -95,5 +102,34 @@ class ADBFunctionalityLearningTest {
 
     }
 
+    @Test
+    fun howToPutRealImagesOntoDevice() {
+
+        //  Put real image onto device in the form of a drawable that already exists.
+        val drawable = getContext().getDrawable(R.drawable.logo)
+        val bitmap = (drawable as BitmapDrawable).bitmap
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        val bitmapdata = stream.toByteArray()
+
+        val testDir = File(Environment.getExternalStorageDirectory().absolutePath + File.separator + "testData")
+        testDir.mkdir()
+
+        val testFile = File(testDir.absolutePath + File.separator + "bitmapExport")
+        ObjectOutputStream(FileOutputStream(testFile)).use {
+            it.writeObject(bitmapdata)
+        }
+
+        val command = "rm -rf ${testDir.absolutePath}"
+        Log.d(TAG, "Delete using command $command")
+        getInstrumentation().getUiAutomation().executeShellCommand(command)
+        await().atMost(5, TimeUnit.SECONDS).until { !testDir.exists() }
+    }
+
+    @Test
+    fun howToGetAtDrawableResourceWithoutStartingActivity() {
+        val drawable = getContext().getDrawable(R.drawable.logo)
+        assertNotNull("Drawable acquisition without activity", drawable)
+    }
 
 }
