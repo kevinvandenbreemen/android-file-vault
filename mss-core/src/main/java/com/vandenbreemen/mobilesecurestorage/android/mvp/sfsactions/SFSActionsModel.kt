8b1fcb467c12cbdfd3d81e89e605fbff7dba3dd2
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-12-28 17:12:13 -0500

    Basic password change functionality

---
+++ mss-core/src/main/java/com/vandenbreemen/mobilesecurestorage/android/mvp/sfsactions/SFSActionsModel.kt
@@ -0,0 +1,29 @@
+package com.vandenbreemen.mobilesecurestorage.android.mvp.sfsactions
+
+import com.vandenbreemen.mobilesecurestorage.android.sfs.SFSCredentials
+import com.vandenbreemen.mobilesecurestorage.patterns.ProgressListener
+import com.vandenbreemen.mobilesecurestorage.patterns.mvp.Model
+import com.vandenbreemen.mobilesecurestorage.security.SecureString
+import com.vandenbreemen.mobilesecurestorage.security.crypto.persistence.SecureFileSystem
+import io.reactivex.Completable
+import io.reactivex.schedulers.Schedulers.computation
+
+class SFSActionsModel(credentials: SFSCredentials): Model(credentials) {
+
+    override fun onClose() {
+
+    }
+
+    override fun setup() {
+
+    }
+
+    fun changePassword(currentPassword: String, newPassword: String, reEnterNewPassword: String, progress: ProgressListener<Long>): Completable {
+        return Completable.create {subscriber ->
+            sfs.changePassword(progress, SecureFileSystem.generatePassword(SecureString.fromPassword(newPassword)))
+            subscriber.onComplete()
+        }.subscribeOn(computation())
+    }
+
+
+}
---
+++ mss-core/src/test/java/com/vandenbreemen/mobilesecurestorage/android/mvp/sfsactions/SFSActionsModelTest.kt
@@ -0,0 +1,85 @@
+package com.vandenbreemen.mobilesecurestorage.android.mvp.sfsactions
+
+import android.os.Environment
+import com.vandenbreemen.mobilesecurestorage.android.sfs.SFSCredentials
+import com.vandenbreemen.mobilesecurestorage.file.ChunkedMediumException
+import com.vandenbreemen.mobilesecurestorage.patterns.ProgressListener
+import com.vandenbreemen.mobilesecurestorage.security.SecureString
+import com.vandenbreemen.mobilesecurestorage.security.crypto.persistence.SecureFileSystem
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.io.File
+
+@RunWith(RobolectricTestRunner::class)
+class SFSActionsModelTest {
+
+    lateinit var credentials: SFSCredentials
+
+    lateinit var sfs: SecureFileSystem
+
+    lateinit var sfsFile: File
+
+    lateinit var model: SFSActionsModel
+
+    val progress = object: ProgressListener<Long> {
+        override fun setMax(progressMax: Long?) {
+
+        }
+
+        override fun update(progress: Long?) {
+
+        }
+    }
+
+    @Before
+    fun setup() {
+        RxJavaPlugins.setComputationSchedulerHandler { scheduler -> AndroidSchedulers.mainThread() }
+
+        sfsFile = File(Environment.getExternalStorageDirectory().toString() + File.separator + "test")
+        val tempPassword = "password"
+        val testPassword = SecureFileSystem.generatePassword(SecureString.fromPassword(tempPassword))
+        credentials = SFSCredentials(sfsFile, testPassword)
+
+        sfs = object : SecureFileSystem(credentials.fileLocation){
+            override fun getPassword(): SecureString = credentials.password
+        }
+
+        model = SFSActionsModel(credentials)
+        model.init().subscribe()
+    }
+
+    @Test
+    fun shouldChangePassword() {
+        //  Act
+        val test = model.changePassword("password", "update", "update", progress).test()
+
+        //  Assert
+        test.assertComplete()
+        test.assertNoErrors()
+
+        try {
+            credentials = SFSCredentials(sfsFile, SecureFileSystem.generatePassword(SecureString.fromPassword("password")))
+            object : SecureFileSystem(credentials.fileLocation){
+                override fun getPassword(): SecureString = credentials.password
+            }
+            fail("System should have changed password")
+        } catch (exc: ChunkedMediumException){
+            System.err.println(exc.localizedMessage)
+        }
+
+        val testPassword = SecureFileSystem.generatePassword(SecureString.fromPassword("update"))
+        credentials = SFSCredentials(sfsFile, testPassword)
+
+        object : SecureFileSystem(credentials.fileLocation){
+            override fun getPassword(): SecureString = credentials.password
+        }.listFiles()
+
+
+    }
+
+}
