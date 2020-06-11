bf905473b86874630a6afbb8b4a3fef3965ddcd5
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-06-10 20:36:27 -0400

    Additional validation

--- secretcamera/src/test/java/com/vandenbreemen/secretcamera/fragment/ConfirmDialogFragmentTest.kt
+++ secretcamera/src/test/java/com/vandenbreemen/secretcamera/fragment/ConfirmDialogFragmentTest.kt
@@ -4,6 +4,7 @@ import android.widget.Button
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.vandenbreemen.secretcamera.R
+import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,7 +17,7 @@ import org.junit.runner.RunWith
 class ConfirmDialogFragmentTest {
 
     @Test
-    fun testStartFragment() {
+    fun `Clicking OK on the dialog calls the callback code`() {
 
         var callBackCalled: Boolean = false
         val factory = ConfirmDialogFragmentFactory(fileNames = listOf("A", "B"), callBack = {
@@ -27,6 +28,19 @@ class ConfirmDialogFragmentTest {
         }
 
         assertTrue(callBackCalled)
+    }
+
+    @Test
+    fun `Dismiss The Confirmation Dialog does not call the callback logic`() {
+        var callBackCalled: Boolean = false
+        val factory = ConfirmDialogFragmentFactory(fileNames = listOf("A", "B"), callBack = {
+            callBackCalled = true
+        })
+        launchFragmentInContainer<ConfirmDeleteDialogFragment>(null, factory = factory).onFragment { fragment ->
+            fragment.view!!.findViewById<Button>(R.id.cancel_button).callOnClick()
+        }
+
+        assertFalse(callBackCalled)
     }
 
 }
