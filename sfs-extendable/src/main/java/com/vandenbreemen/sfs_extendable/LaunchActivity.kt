0b50c6df2632d684f93822b442fecc0e34036ed0
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-06-28 08:31:23 -0400

    Start up a new project module to look into using my new design patterns for developing SFS apps

---
+++ sfs-extendable/src/main/java/com/vandenbreemen/sfs_extendable/LaunchActivity.kt
@@ -0,0 +1,12 @@
+package com.vandenbreemen.sfs_extendable
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
